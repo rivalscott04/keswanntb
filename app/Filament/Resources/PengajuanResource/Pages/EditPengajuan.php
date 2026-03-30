6b56aa32aa842f42f7f290c7d4f865460fcfec75
Ridwan <ridwan@tablogs.com.au>
2026-03-30 11:32:09 +0800

    Add mutation logic in EditPengajuan, EditPengajuanPemasukan, and EditPengajuanPengeluaran to handle custom port entries. Update 'Lainnya' fields to reflect user input for 'pelabuhan_asal' and 'pelabuhan_tujuan', improving data consistency before saving.

--- app/Filament/Resources/PengajuanResource/Pages/EditPengajuan.php
+++ app/Filament/Resources/PengajuanResource/Pages/EditPengajuan.php
@@ -10,6 +10,21 @@ class EditPengajuan extends EditRecord
 {
     protected static string $resource = PengajuanResource::class;
 
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (isset($data['pelabuhan_asal']) && $data['pelabuhan_asal'] === 'Lainnya' && isset($data['pelabuhan_asal_lainnya'])) {
+            $data['pelabuhan_asal'] = $data['pelabuhan_asal_lainnya'];
+        }
+        unset($data['pelabuhan_asal_lainnya']);
+
+        if (isset($data['pelabuhan_tujuan']) && $data['pelabuhan_tujuan'] === 'Lainnya' && isset($data['pelabuhan_tujuan_lainnya'])) {
+            $data['pelabuhan_tujuan'] = $data['pelabuhan_tujuan_lainnya'];
+        }
+        unset($data['pelabuhan_tujuan_lainnya']);
+
+        return $data;
+    }
+
     protected function getHeaderActions(): array
     {
         return [
